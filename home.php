a3d06444b990366b319fbce850a5a8a9cb3de4b8
Victor Ribeiro <v.leitteribeiro@gmail.com>
2022-01-30 21:26:21 -0300

    Enviando o projeto

---
+++ home.php
@@ -0,0 +1,79 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="assets/css/home.css">
+    <title>BookFlix - Home</title>
+</head>
+
+<body>
+    <nav class="nav">
+        <a href="#">BOOKFLIX</a>
+        <ul class="nav__list">
+            <li class="nav__list_items">Gênero</li>
+            <li class="nav__list_items">Aleatório</li>
+            <li class="nav__list_items">Nacionais</li>
+        </ul>
+    </nav>
+
+    <div class="main">
+        <div class="container">
+            <div class="card__books">
+                <div class="books">
+                    <img class="books__image" src="assets/images/miranha.jpg" alt="">
+                </div>
+                <div class="books">
+                    <img class="books__image" src="assets/images/resident.jpg" alt="">
+                </div>
+                <div class="books">
+                    <img class="books__image" src="assets/images/venom.jpg" alt="">
+                </div>
+                <div class="books">
+                    teste
+                </div>
+                <div class="books">
+                    teste
+                </div>
+            </div>
+            <div class="card__books">
+                <div class="books">
+                <img class="books__image" src="assets/images/venom.jpg" alt="">
+                </div>
+                <div class="books">
+                    teste
+                </div>
+                <div class="books">
+                    teste
+                </div>
+                <div class="books">
+                    teste
+                </div>
+                <div class="books">
+                    teste
+                </div>
+            </div>
+            <div class="card__books">
+                <div class="books">
+                    teste
+                </div>
+                <div class="books">
+                    teste
+                </div>
+                <div class="books">
+                    teste
+                </div>
+                <div class="books">
+                    teste
+                </div>
+                <div class="books">
+                    teste
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+
+</html>
---
+++ login.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="assets/css/login.css">
+    <title>BookFlix - Login</title>
+</head>
+
+<body>
+    <header class="header">
+        <div class="logo">
+            <h1>BOOKFLIX</h1>
+        </div>
+    </header>
+    <div class="container">
+        <div class="card__user">
+            <div class="photo__user" style="background-color: #044E92;">
+                <!-- <img src="" alt="user-1"> -->
+            </div>
+            <div class="name__user">
+                <h3>User 1</h3>
+            </div>
+        </div>
+        <div class="card__user">
+            <div class="photo__user" style="background-color: #C00707;">
+                <!-- <img src="" alt="user-1"> -->
+            </div>
+            <div class="name__user">
+                <h3>User 2</h3>
+            </div>
+        </div>
+        <div class="card__user">
+            <div class="photo__user" style="background-color: #6B0C67;">
+                <!-- <img src="" alt="user-1"> -->
+            </div>
+            <div class="name__user">
+                <h3>User 3</h3>
+            </div>
+        </div>
+        <div class="card__user">
+            <div class="photo__user" style="background-color: #cf5a00;">
+                <!-- <img src="" alt="user-1"> -->
+            </div>
+            <div class="name__user">
+                <h3>User 4</h3>
+            </div>
+        </div>
+    </div>
+</body>
+
+</html>
