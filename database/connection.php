fddf4a70a10f959008a891976a9f99cb96406d52
Victor Ribeiro <v.leitteribeiro@gmail.com>
2022-02-07 00:51:26 -0300

    Atualizações

--- database/connection.php
+++ database/connection.php
@@ -5,6 +5,7 @@ $db = "books";
 $user = "root";
 $pass = "";
 
+
 try {
     $conn = new PDO("mysql:host=$host;dbname=$db;charset=utf8", $user, $pass);
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
