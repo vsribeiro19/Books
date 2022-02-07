272ec5b7fa9a8026bda2a7245248ac59ffb3390c
Victor Ribeiro <v.leitteribeiro@gmail.com>
2022-02-07 00:41:11 -0300

    Inserindo o Back End no projeto

--- login.php
+++ login.php
@@ -1,54 +1,5 @@
-<!DOCTYPE html>
-<html lang="pt-br">
+<?php
+include_once './database/connection.php';
+$dados = filter_input_array(INPUT_POST,FILTER_DEFAULT);
+var_dump($dados);
 
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="assets/css/login.css">
-    <title>BookFlix - Login</title>
-</head>
-
-<body>
-    <header class="header">
-        <div class="logo">
-            <h1>BOOKFLIX</h1>
-        </div>
-    </header>
-    <div class="container">
-        <div class="card__user">
-            <div class="photo__user" style="background-color: #044E92;">
-                <!-- <img src="" alt="user-1"> -->
-            </div>
-            <div class="name__user">
-                <h3>User 1</h3>
-            </div>
-        </div>
-        <div class="card__user">
-            <div class="photo__user" style="background-color: #C00707;">
-                <!-- <img src="" alt="user-1"> -->
-            </div>
-            <div class="name__user">
-                <h3>User 2</h3>
-            </div>
-        </div>
-        <div class="card__user">
-            <div class="photo__user" style="background-color: #6B0C67;">
-                <!-- <img src="" alt="user-1"> -->
-            </div>
-            <div class="name__user">
-                <h3>User 3</h3>
-            </div>
-        </div>
-        <div class="card__user">
-            <div class="photo__user" style="background-color: #cf5a00;">
-                <!-- <img src="" alt="user-1"> -->
-            </div>
-            <div class="name__user">
-                <h3>User 4</h3>
-            </div>
-        </div>
-    </div>
-</body>
-
-</html>
--- register.php
+++ register.php
@@ -1,12 +1,10 @@
-<!DOCTYPE html>
-<html lang="pt-br">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Cadastre-se</title>
-</head>
-<body>
-    
-</body>
-</html>
+<?php
+include_once './database/connection.php';
+$dados = filter_input_array(INPUT_POST,FILTER_DEFAULT);
+var_dump($dados);
+
+if(!empty($dados['cadastrar'])){
+    var_dump($dados);
+    "INSERT INTO register (nome, email, senha) VALUES (:nome, :email, :senha)";
+
+}
